44aa574b9824dd5e51e875c11c96c8e09e3b7a8d
Anthony Cera <ace_cera@yahoo.com>
2018-03-12 20:23:21 -0700

    added three entries into db table.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Western Bacon Cheeseburger", fasle);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Spicy Chicken Sandwich", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Shrimp and Pork Curry Burger", devoured);
